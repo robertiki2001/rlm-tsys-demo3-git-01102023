a447d9c6a81ffd58e338e91c766fe1a113fd59d7
Robert <robertrlm16@gmail.com>
2023-10-01 20:54:59 +0200

    Subiendo proyecto 3

---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,9 @@
+public class ComentarioVariasLineas {
+	
+		public static void main (String[] args) {
+		for(int i=0;i<6;i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+		}
+		
+}
